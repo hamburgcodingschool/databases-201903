8a63aa2092506522cc733dbb6f7f0e67faf18116
Teresa Holfeld <teresa.holfeld@gmail.com>
2019-03-22 15:30:47 +0100

    PUT request for student

--- example/routes/api.js
+++ example/routes/api.js
@@ -6,11 +6,47 @@ const router = vertex.router()
 const Student = require('../model/Student')
 
 router.get('/student', (req, res) => {
-	Student.find()
+	const query = req.query
+
+	Student.find(query)
 		.then(students => {
 			res.json({
 				confirmation: 'success',
 				data: students
+			})
+		})
+		.catch(err => {
+			res.json({
+				confirmation: 'fail',
+				data: err.message
+			})
+		})
+})
+
+router.get('/student/:id', (req, res) => {
+	const id = req.params.id
+
+	Student.findById(id)
+		.then(student => {
+			res.json({
+				confirmation: 'success',
+				data: student
+			})
+		})
+		.catch(err => {
+			res.json({
+				confirmation: 'fail',
+				data: err.message
+			})
+		})
+})
+
+router.post('/student', (req, res) => {
+	Student.create(req.body)
+		.then(student => {
+			res.json({
+				confirmation: 'success',
+				data: student
 			})
 		})
 		.catch(err => {
